35f9e480fdc1c52cf184da920e4fd955a0947703
thrdchld <thirdchilddesigner@gmail.com>
2025-12-14 18:28:37 +0700

    Enable WebView debugging for Chrome DevTools remote inspection

--- android/app/src/main/java/com/klipper/app/MainActivity.java
+++ android/app/src/main/java/com/klipper/app/MainActivity.java
@@ -1,11 +1,15 @@
 package com.klipper.app;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        // Enable WebView debugging for Chrome DevTools
+        WebView.setWebContentsDebuggingEnabled(true);
+        
         registerPlugin(FFmpegPlugin.class);
         super.onCreate(savedInstanceState);
     }
